4c109320e443c97fa4e54a2ca3598bd780866b68
Xethyr <monteiv@me.com>
2024-06-22 00:23:06 -0500

    Added keyboard functionality

--- script.js
+++ script.js
@@ -75,12 +75,16 @@ window.addEventListener('keydown', (e) => { // Adds keyboard functionality
     const operatorInput = e.key;
     const allowedInputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '.'];
     if (allowedInputs.includes(e.key)) {
+        if (e.key === "+" || e.key === "-" || e.key === '*' || e.key === '/') {
+            appendToDisplay(' ' + e.key + ' ');
+        } else {
         appendToDisplay(e.key);
+        }
     }
     if (e.key === 'c') {
         clearDisplay();
     }
     if (e.key === 'Enter') {
-        calculate();
+        getValues();
     }
 })
